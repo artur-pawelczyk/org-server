d8f9610e767736dad8ed057fde14a79d5ca0531c
Artur Pawelczyk <pchairman@gmail.com>
2023-08-31 22:59:47 +0200

    Fix integration tests

--- tests/http_test.rs
+++ tests/http_test.rs
@@ -1,69 +1,69 @@
+use std::sync::atomic::{AtomicU16, Ordering};
+
 use org_server::{empty_doc::EmptyOrgSource, doc::{OrgSource, StaticOrgSource}};
 use reqwest::StatusCode;
-use tokio::task::JoinHandle;
+
 
 #[tokio::test]
 async fn test_connect() {
-    let handle = prepare_server(EmptyOrgSource).await;
+    let TestServer { port } = prepare_server(EmptyOrgSource).await;
 
-    let resp = reqwest::get("http://0.0.0.0:8080")
+    let resp = reqwest::get(format!("http://0.0.0.0:{port}"))
         .await.unwrap();
 
     assert_eq!(resp.status(), StatusCode::OK);
-
-    // TODO: Provide a better setup/teardown logic
-    handle.abort();
 }
 
 #[tokio::test]
 async fn test_doc_not_found() {
-    let handle = prepare_server(EmptyOrgSource).await;
+    let TestServer { port } = prepare_server(EmptyOrgSource).await;
 
-    let resp = reqwest::get("http://0.0.0.0:8080/tasks.org")
+    let resp = reqwest::get(format!("http://0.0.0.0:{port}/tasks.org"))
         .await.unwrap();
 
     assert_eq!(resp.status(), StatusCode::NOT_FOUND);
-
-    handle.abort();
 }
 
 #[tokio::test]
 async fn test_static_org_source() {
     let mut source = StaticOrgSource::default();
     source.add_doc("tasks.org", "the content");
-    let handle = prepare_server(source).await;
+    let TestServer { port } = prepare_server(source).await;
 
-    let resp = reqwest::get("http://0.0.0.0:8080/")
-        .await.unwrap();
+    let resp = reqwest::get(format!("http://0.0.0.0:{port}/")).await.unwrap();
     assert_eq!(resp.status(), StatusCode::OK);
     let text = resp.text().await.unwrap();
     assert!(text.contains("tasks.org"));
 
-    let resp = reqwest::get("http://0.0.0.0:8080/tasks.org")
-        .await.unwrap();
+    let resp = reqwest::get(format!("http://0.0.0.0:{port}/tasks.org")).await.unwrap();
     assert_eq!(resp.status(), StatusCode::OK);
     let text = resp.text().await.unwrap();
     assert!(text.contains("the content"));
+}
 
+static PORT_NUMBER: AtomicU16 = AtomicU16::new(8000);
 
-    handle.abort();
+struct TestServer {
+    port: u16,
 }
 
 #[must_use]
-async fn prepare_server(source: impl OrgSource + 'static) -> JoinHandle<()> {
-    let app = org_server::server::Server{ port: 8080 };
-    let handle = tokio::spawn(async move {
+async fn prepare_server(source: impl OrgSource + 'static) -> TestServer {
+    let port = PORT_NUMBER.fetch_add(1, Ordering::Relaxed);
+    println!("using port: {port}");
+    let app = org_server::server::Server{ port };
+    tokio::spawn(async move {
         app.start(Box::new(source)).await.unwrap();
     });
 
-    wait_for_server().await;
-
-    handle
+    let server = TestServer { port };
+    wait_for_server(&server).await;
+    server
 }
 
-async fn wait_for_server() {
+async fn wait_for_server(TestServer { port }: &TestServer) {
     for _ in 0..5 {
-        match reqwest::get("http://0.0.0.0:8080").await {
+        match reqwest::get(format!("http://0.0.0.0:{port}")).await {
             Ok(_) => return,
             Err(_) => continue,
         }
